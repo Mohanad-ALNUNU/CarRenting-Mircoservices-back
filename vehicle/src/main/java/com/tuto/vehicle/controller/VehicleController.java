7827cb954e8d22cb171c5d50b5a95af2a73f1791
Mohanad-ALNUNU <mohanadalnunu@gmail.com>
2023-05-26 21:46:20 +0100

    fix: add application layer

--- vehicle/src/main/java/com/tuto/vehicle/controller/VehicleController.java
+++ vehicle/src/main/java/com/tuto/vehicle/controller/VehicleController.java
@@ -2,7 +2,7 @@ package com.tuto.vehicle.controller;
 
 import com.tuto.vehicle.entity.Vehicle;
 import com.tuto.vehicle.repository.VehicleRepository;
-import com.tuto.vehicle.service.VehicleService;
+import com.tuto.vehicle.application.VehicleApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,51 +16,51 @@ import java.util.Optional;
 @RequestMapping("/api/vehicles")
 public class VehicleController {
 
-    private final VehicleService vehicleService;
+    private final VehicleApplicationService vehicleApplicationService;
 
     @Autowired
-    public VehicleController(VehicleService vehicleService) {
+    public VehicleController(VehicleApplicationService vehicleApplicationService) {
 
-        this.vehicleService = vehicleService;
+        this.vehicleApplicationService = vehicleApplicationService;
     }
 
     @PostMapping
     public ResponseEntity<String> createVehicle(@RequestBody Vehicle vehicle) {
-            vehicleService.sendVehicleEntity(vehicle);
+            vehicleApplicationService.createVehicle(vehicle);
         return new  ResponseEntity<>("Vehicle sent", HttpStatus.OK);
     }
 
     @GetMapping("/all")
     public ResponseEntity<List<Vehicle>> getAllVehicles() {
-        return vehicleService.getAllVehicles();
+        return vehicleApplicationService.getAllVehicles();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Vehicle> getVehicleById(@PathVariable("id") String id) {
-        return vehicleService.getVehicleById(id);
+        return vehicleApplicationService.getVehicleById(id);
     }
 
     @GetMapping("/search/vehicleName/{name}")
     public ResponseEntity<List<Vehicle>> getVehicleByName(@PathVariable("name") String name) {
-        return vehicleService.getVehicleByName(name);
+        return vehicleApplicationService.getVehicleByName(name);
     }
 
     @GetMapping("/search/vehicleModelDate/{modelDate}")
     public ResponseEntity<List<Vehicle>> getVehicleByModelDate(@PathVariable("modelDate") int modelDate) {
-        return vehicleService.getVehicleByModelDate(modelDate);
+        return vehicleApplicationService.getVehicleByModelDate(modelDate);
     }
     @GetMapping("/search/vehiclesNameModelDate/{name}/{modelDate}")
     public ResponseEntity<List<Vehicle>> getVehiclesByNameAndModelDate(@PathVariable("name") String name, @PathVariable("modelDate") int modelDate) {
-        return vehicleService.getVehiclesByNameAndModelDate(name, modelDate);
+        return vehicleApplicationService.getVehiclesByNameAndModelDate(name, modelDate);
     }
 
     @PutMapping("/update/{id}")
     public ResponseEntity<Vehicle> updateVehicle(@PathVariable("id") String id, @RequestBody Vehicle updatedVehicle) {
-        return vehicleService.updateVehicle(id, updatedVehicle);
+        return vehicleApplicationService.updateVehicle(id, updatedVehicle);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteVehicle(@PathVariable("id") String id) {
-        return vehicleService.deleteVehicle(id);
+        return vehicleApplicationService.deleteVehicle(id);
     }
 }
